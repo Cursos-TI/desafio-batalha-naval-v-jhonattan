07d6ffdaf6e36b0babe6d5d9bd0159b6de603ca4
Jhonattan Azevedo <123788903+v-jhonattan@users.noreply.github.com>
2026-03-01 19:02:43 +0000

    parte 03
    Updated game level and added skill features for the naval battle.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -11,8 +11,8 @@
 
 int main() {
 
-    // Batalha Naval - Nivel Aventureiro 
-    // Matriz 10x10 que representa o tabuleiro (0 representa agua e 3 o navio)
+    // Batalha Naval - Nivel Mestre 
+    // Matriz 10x10 que representa o tabuleiro (0 representa agua, 3 o navio e 1 a Habilidade)
     
     int tabuleiro[LINHAS][COLUNAS];
     int tamanhoNavio = 3;
@@ -45,6 +45,42 @@ int main() {
     for (int h = 0; h < tamanhoNavio; h++) {
         tabuleiro[6 + h][1 + h] = 3;
     }
+
+    
+    // --- Habilidades ---
+
+    // Em Cone (Inicia na linha 7, coluna 4)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j >= (2 - i) && j <= (2 + i)) {
+                tabuleiro[7 + i][2 + j] = 1;
+            }
+        }
+    }
+
+    // Em Cruz (Inicia na linha 7, coluna 4)
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            if (i == 4 || j == 4) {
+                if ((i >= 2 && i <= 6 && j == 4) || (j >= 2 && j <= 6 && i == 4)) {
+                    tabuleiro[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    // Em Octaedro (Centro na linha 2, coluna 5)
+    int centroI = 1, centroJ = 4;
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            int distI = (i >= centroI) ? i - centroI : centroI - i;
+            int distJ = (j >= centroJ) ? j - centroJ : centroJ - j;
+            if (distI + distJ <= 2) {
+                tabuleiro[i][j] = 1;
+            }
+        }
+    }
+
 
 
     // Exibição do Tabuleiro 
